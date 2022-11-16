7895d1614888d63a28f40df855a2da66dc39c02d
wey1998 <yitwey1998@hotmail.com>
2022-11-17 00:35:43 +0800

    Completed milestone 5- task 3

--- camera_rps.py
+++ camera_rps.py
@@ -9,8 +9,8 @@ def get_computer_choice():
     return comp_choice
 
 def get_prediction():
-    model = load_model('keras_model.h5')
-    cap = cv2.VideoCapture(0)
+    #model = load_model('keras_model.h5')
+    #cap = cv2.VideoCapture(0)
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
     
     # labels = []
@@ -24,13 +24,12 @@ def get_prediction():
     current_time = time.time()
     end_time = time.time() + time_added
 
-    
     while True:
         current_time = time.time()
         ret, frame = cap.read() 
         cv2.imshow('frame', frame)
         
-        if end_time - current_time <= 0.05 and end_time - current_time > 0:
+        if end_time - current_time <= 0.05 and end_time - current_time > -0.05:
             print("shoot")
             ret, frame = cap.read() 
             resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
@@ -68,23 +67,57 @@ def get_prediction():
 
 def get_winner(user_choice,computer_choice):
     
+    user_wins = 0 
+    computer_wins = 0
+    
     if user_choice == "scissors" and computer_choice== "paper":
         print("You won!")
+        user_wins += 1
+        return [user_wins, computer_wins]
     elif user_choice == "rock" and computer_choice == "scissors":
         print("You won!")
+        user_wins += 1
+        return [user_wins, computer_wins]
     elif user_choice == "paper" and computer_choice == "rock":
         print("You won!")
+        user_wins += 1
+        return [user_wins, computer_wins]
     elif user_choice == computer_choice:      
         print("It is a tie!")
+        return [user_wins, computer_wins]
+    elif user_choice == "nothing":
+        print("You did not show anything")
     else:
         print("You lost")
+        computer_wins += 1
+        return [user_wins, computer_wins]
         
 def play():
-    comp_choice = get_computer_choice()
-    user_pred = get_prediction()
-    print(f"you chose {user_pred} and computer chose {comp_choice}")
-    get_winner(user_pred,comp_choice)
-
-
+    user_score = 0
+    computer_score = 0
+    global cap, model
+    cap = cv2.VideoCapture(0)
+    model = load_model('keras_model.h5')
+    while True:
+        comp_choice = get_computer_choice()
+        user_pred = get_prediction()
+        print(f"you chose {user_pred} and computer chose {comp_choice}")
+        winner = get_winner(user_pred,comp_choice)
+        print(winner)
+        if winner[0] == 1:
+            user_score += 1
+            print(f'Your score is {user_score} and the computer score is {computer_score}')
+            if user_score == 3:
+                break
+        elif winner[1] == 1:
+            computer_score += 1
+            print(f'Your score is {user_score} and the computer score is {computer_score}')
+            if computer_score == 3:
+                break
+            
+            
+    cap.release()
+    cv2.destroyAllWindows()  
+            
 play()
 
