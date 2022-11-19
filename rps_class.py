6e72b7a64357f3f2d0f9c1a23140df5e92140409
wey1998 <yitwey1998@hotmail.com>
2022-11-20 02:02:41 +0800

    Update rps_class.py

--- rps_class.py
+++ rps_class.py
@@ -1,6 +1,20 @@
+import random
+import cv2
+from keras.model import load_model
+import numpy as np
+import time
+
 class rps:
     
-    def __init__(self,computer_score,user_score):
-        self.computer_score = computer_score
-        self.user_score = user_score
+    def __init__(self):
+        self.computer_score = 0
+        self.user_score = 0
+        self.cap = cv2.VideoCapture(0)
+        self.model = load_model("keras_model.h5")
         
+    def get_computer_choice(self):
+        comp_choice = random.choice(["rock","paper","scissors"])
+        return comp_choice
+    
+    def get_prediction():
+        pass
